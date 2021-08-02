814ed8ebde8d00a589186dfa6e32a8a2c80ee189
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-08-02 22:36:55 +0500

    fix: columns utility

--- packages/Utilities/src/Columns.ts
+++ packages/Utilities/src/Columns.ts
@@ -29,28 +29,7 @@ const ColumnsUtility = (
   props: ColumnsUtilityProps
 ): ColumnsUtilityReturnProps => {
   const { xs = '', sm = '', md = '', lg = '', xl = '' } = props;
-  let column = '';
-  if (xl && lg && md && sm && xs) {
-    column = `${xl}|${lg}|${md}|${sm}|${xs}`;
-  } else if (xl && lg && md && sm && !xs) {
-    column = `${xl}|${lg}|${md}|${sm}|${sm}`;
-  } else if (xl && lg && md && !sm && !xs) {
-    column = `${xl}|${lg}|${md}|${md}|${md}`;
-  } else if (xl && lg && !md && !sm && !xs) {
-    column = `${xl}|${lg}|${lg}|${lg}|${lg}`;
-  } else if (xl && !lg && !md && !sm && !xs) {
-    column = `${xl}|${xl}|${xl}|${xl}|${xl}`;
-  } else if (!xl && lg && !md && !sm && !xs) {
-    column = `${lg}|${lg}|${lg}|${lg}|${lg}`;
-  } else if (!xl && !lg && md && !sm && !xs) {
-    column = `${md}|${md}|${md}|${md}|${md}`;
-  } else if (!xl && !lg && !md && sm && !xs) {
-    column = `${sm}|${sm}|${sm}|${sm}|${sm}`;
-  } else if (!xl && !lg && !md && !sm && xs) {
-    column = `${xs}|${xs}|${xs}|${xs}|${xs}`;
-  } else {
-    column = `${xl}|${lg}|${md}|${sm}|${xs}`;
-  }
+  let column = `${xl}|${lg}|${md}|${sm}|${xs}`;
   return { column };
 };
 
